52a32d341c15d37e7281f1bc937bf30d62e92ddf
Geoffrey Yu <geoffrey.yu96@icloud.com>
2022-08-29 15:13:45 +0200

    now figsize can be customised in order to avoid kernel crashes

--- alphapulldown/analysis_pipeline/utils.py
+++ alphapulldown/analysis_pipeline/utils.py
@@ -7,11 +7,11 @@ import os
 from af2plots.plotter import plotter
 
 
-def display_pae_plots(subdir):
+def display_pae_plots(subdir,figsize=(450, 450)):
     """A function to display all the pae plots in the subdir"""
     images = sorted([i for i in os.listdir(subdir) if ".png" in i])
     if len(images) > 0:
-        fig, axs = plt.subplots(1, len(images), figsize=(450, 450))
+        fig, axs = plt.subplots(1, len(images), figsize=figsize)
         for i in range(len(images)):
             img = plt.imread(os.path.join(subdir, images[i]))
             axs[i].imshow(img)
