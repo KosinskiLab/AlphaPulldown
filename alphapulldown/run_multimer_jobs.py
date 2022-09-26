7582c8e89215c921ab79566dc6184d95518ad096
Geoffrey Yu <geoffrey.yu96@icloud.com>
2022-09-26 14:46:03 +0200

    replace the old version with the one on the main

--- alphapulldown/run_multimer_jobs.py
+++ alphapulldown/run_multimer_jobs.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
+
 # Author: Dingquan Yu
 # A script to create region information for create_multimer_features.py
 # #
+
 
 import itertools
 from re import I
@@ -14,6 +16,8 @@ import os
 import shutil
 from pathlib import Path
 from alphapulldown.predict_structure import predict
+
+
 
 
 flags.DEFINE_enum(
@@ -72,8 +76,13 @@ flags.DEFINE_integer(
 flags.DEFINE_integer(
     "job_index", None, "index of sequence in the fasta file, starting from 1"
 )
+flags.DEFINE_boolean(
+    "no_pair_msa", False, "do not pair the MSAs when constructing multimer objects"
+)
 flags.mark_flag_as_required("output_path")
 FLAGS = flags.FLAGS
+
+
 
 
 def create_pulldown_info(
@@ -91,6 +100,7 @@ def create_pulldown_info(
     num_cols = len(candidate_proteins) + 1
     data = dict()
 
+
     if job_index is None:
         for i in range(num_cols):
             curr_col = []
@@ -99,12 +109,15 @@ def create_pulldown_info(
             update_dict = {f"col_{i+1}": curr_col}
             data.update(update_dict)
 
+
     elif isinstance(job_index, int):
         target_pair = all_protein_pairs[job_index-1]
         for i in range(num_cols):
             update_dict = {f"col_{i+1}": [target_pair[i]]}
             data.update(update_dict)
     return data
+
+
 
 
 def create_all_vs_all_info(all_proteins: list, job_index=None):
@@ -116,14 +129,18 @@ def create_all_vs_all_info(all_proteins: list, job_index=None):
     else:
         combs = all_possible_pairs
 
+
     col1 = []
     col2 = []
     for comb in combs:
         col1.append(comb[0])
         col2.append(comb[1])
 
+
     data = {"col1": col1, "col2": col2}
     return data
+
+
 
 
 def create_custom_info(all_proteins):
@@ -137,7 +154,9 @@ def create_custom_info(all_proteins):
     return data
 
 
-def create_multimer_objects(data, monomer_objects_dir):
+
+
+def create_multimer_objects(data, monomer_objects_dir, pair_msa=True):
     """
     A function to create multimer objects
 
@@ -146,25 +165,29 @@ def create_multimer_objects(data, monomer_objects_dir):
     monomer_objects_dir: a directory where pre-computed monomer objects are stored
     """
 
+
     multimers = []
     num_jobs = len(data[list(data.keys())[0]])
     job_idxes = list(range(num_jobs))
 
+
     for job_idx in job_idxes:
         interactors = create_interactors(data, monomer_objects_dir, job_idx)
         if len(interactors) > 1:
-            multimer = MultimericObject(interactors=interactors)
-            multimer.create_all_chain_features()
+            multimer = MultimericObject(interactors=interactors,pair_msa=pair_msa)
             logging.info(f"done creating multimer {multimer.description}")
             multimers.append(multimer)
         else:
             logging.info(f"done loading monomer {interactors[0].description}")
             multimers.append(interactors[0])
 
+
     return multimers
 
 
-def create_homooligomers(oligomer_state_file, monomer_objects_dir, job_index=None):
+
+
+def create_homooligomers(oligomer_state_file, monomer_objects_dir, job_index=None, pair_msa = False):
     """a function to read homooligomer state"""
     multimers = []
     monomer_dir_dict = make_dir_monomer_dictionary(monomer_objects_dir)
@@ -174,6 +197,7 @@ def create_homooligomers(oligomer_state_file, monomer_objects_dir, job_index=Non
             job_idxes = [job_index - 1]
         else:
             job_idxes = list(range(len(lines)))
+
 
         for job_idx in job_idxes:
             l = lines[job_idx]
@@ -185,12 +209,12 @@ def create_homooligomers(oligomer_state_file, monomer_objects_dir, job_index=Non
                     protein_name = l.rstrip().split(",")[0]
                     num_units = 1
 
+
                 if num_units > 1:
                     monomer = load_monomer_objects(monomer_dir_dict, protein_name)
                     interactors = [monomer] * num_units
-                    homooligomer = MultimericObject(interactors)
+                    homooligomer = MultimericObject(interactors,pair_msa=pair_msa)
                     homooligomer.description = f"{protein_name}_homo_{num_units}er"
-                    homooligomer.create_all_chain_features()
                     multimers.append(homooligomer)
                     logging.info(
                         f"finished creating homooligomer {homooligomer.description}"
@@ -203,7 +227,9 @@ def create_homooligomers(oligomer_state_file, monomer_objects_dir, job_index=Non
     return multimers
 
 
-def create_custom_jobs(custom_input_file, monomer_objects_dir, job_index=None):
+
+
+def create_custom_jobs(custom_input_file, monomer_objects_dir, job_index=None, pair_msa=True):
     """
     A function to create multimers under custom mode
 
@@ -221,13 +247,16 @@ def create_custom_jobs(custom_input_file, monomer_objects_dir, job_index=None):
     else:
         job_idxes = list(range(len(lines)))
 
+
     for job_idx in job_idxes:
         l = lines[job_idx]
         if len(l.strip()) > 0:
             all_proteins = read_custom(l)
             data = create_custom_info(all_proteins)
-            multimers = create_multimer_objects(data, monomer_objects_dir)
+            multimers = create_multimer_objects(data, monomer_objects_dir, pair_msa=pair_msa)
     return multimers
+
+
 
 
 def predict_individual_jobs(multimer_object, output_path, model_runners, random_seed):
@@ -235,8 +264,10 @@ def predict_individual_jobs(multimer_object, output_path, model_runners, random_
     Path(output_path).mkdir(parents=True, exist_ok=True)
     logging.info(f"now running prediction on {multimer_object.description}")
 
+
     if not isinstance(multimer_object, MultimericObject):
         multimer_object.input_seqs = [multimer_object.sequence]
+
 
     predict(
         model_runners,
@@ -251,6 +282,8 @@ def predict_individual_jobs(multimer_object, output_path, model_runners, random_
     create_and_save_pae_plots(multimer_object, output_path)
 
 
+
+
 def predict_multimers(multimers):
     """
     Final function to predict multimers
@@ -260,6 +293,7 @@ def predict_multimers(multimers):
     or create_custom_jobs() or create_homooligomers()
     """
     for object in multimers:
+        logging.info('object: '+object.description)
         if isinstance(object, MultimericObject):
             model_runners, random_seed = create_model_runners_and_random_seed(
                 "multimer",
@@ -291,8 +325,11 @@ def predict_multimers(multimers):
             )
 
 
+
+
 def main(argv):
     check_output_dir(FLAGS.output_path)
+
 
     if FLAGS.mode == "pulldown":
         bait_proteins = read_all_proteins(FLAGS.protein_lists[0])
@@ -302,26 +339,33 @@ def main(argv):
         data = create_pulldown_info(
             bait_proteins, candidate_proteins, job_index=FLAGS.job_index
         )
-        multimers = create_multimer_objects(data, FLAGS.monomer_objects_dir)
+        multimers = create_multimer_objects(data, FLAGS.monomer_objects_dir, not FLAGS.no_pair_msa)
+
 
     elif FLAGS.mode == "all_vs_all":
         all_proteins = read_all_proteins(FLAGS.protein_lists[0])
         data = create_all_vs_all_info(all_proteins, job_index=FLAGS.job_index)
-        multimers = create_multimer_objects(data, FLAGS.monomer_objects_dir)
+        multimers = create_multimer_objects(data, FLAGS.monomer_objects_dir, not FLAGS.no_pair_msa)
+
 
     elif FLAGS.mode == "homo-oligomer":
         multimers = create_homooligomers(
             FLAGS.oligomer_state_file,
             FLAGS.monomer_objects_dir,
             job_index=FLAGS.job_index,
+            pair_msa=not FLAGS.no_pair_msa
         )
+
 
     elif FLAGS.mode == "custom":
         multimers = create_custom_jobs(
-            FLAGS.protein_lists, FLAGS.monomer_objects_dir, job_index=FLAGS.job_index
+            FLAGS.protein_lists, FLAGS.monomer_objects_dir, job_index=FLAGS.job_index, pair_msa=not FLAGS.no_pair_msa
         )
 
+
     predict_multimers(multimers)
+
+
 
 
 if __name__ == "__main__":
