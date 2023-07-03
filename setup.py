215c47db381e03b0ed143e10205120a6ca8e7f12
Dmitry Molodenskiy <dmolodenskiy@login01.cluster.embl.de>
2023-07-03 16:06:49 +0200

    Reverted to Geoffrey's version

--- setup.py
+++ setup.py
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
 from setuptools import setup
 if __name__ == '__main__':
     setup()
-=======
-from setuptools import setup, find_packages
-if __name__ == '__main__':
-    setup(name="TrueMultimer",
-          version="0.1",
-          packages=find_packages(include=["alphafold*"]),
-          )
->>>>>>> 4425307c0570304cfdf3411b13525817ebebb01b
