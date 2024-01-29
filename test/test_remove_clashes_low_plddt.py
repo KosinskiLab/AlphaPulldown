231863af7faa61fa04d45829c90a3bab9d9e2ff2
Dima Molodenskiy <dmolodenskiy@embl-hamburg.de>
2024-01-29 11:29:13 +0100

    Use another template as AF input file

--- test/test_remove_clashes_low_plddt.py
+++ test/test_remove_clashes_low_plddt.py
@@ -6,28 +6,29 @@ from pathlib import Path
 
 alphapulldown_dir = os.path.dirname(alphapulldown.__file__)
 alphapulldown_dir = Path(alphapulldown_dir)
-cif_file = alphapulldown_dir / '..' / 'test' / 'test_data' / 'true_multimer' / 'cage_BC_AF.cif'
+pdb_file = alphapulldown_dir / '..' / 'test' / 'test_data' / 'true_multimer' / 'ranked_0.pdb'
 
 
 def test_init():
     for chain in ['B', 'C']:
-        mmcif_chain = MmcifChainFiltered(cif_file, "TEST", chain)
-        assert mmcif_chain.input_file_path == cif_file
+        mmcif_chain = MmcifChainFiltered(pdb_file, "TEST", chain)
+        assert mmcif_chain.input_file_path == pdb_file.with_suffix('.cif')
         assert mmcif_chain.chain_id == chain
 
 
 def test_eq():
-    mmcif_chain1 = MmcifChainFiltered(cif_file, "TEST", "B")
-    mmcif_chain2 = MmcifChainFiltered(cif_file, "TEST", "B")
+    mmcif_chain1 = MmcifChainFiltered(pdb_file, "TEST", "B")
+    mmcif_chain2 = MmcifChainFiltered(pdb_file, "TEST", "B")
     assert mmcif_chain1 == mmcif_chain2
 
 
 def test_remove_clashes():
     for chain in ['B', 'C']:
-        mmcif_chain = MmcifChainFiltered(cif_file, "TEST", chain)
+        mmcif_chain = MmcifChainFiltered(pdb_file, "TEST", chain)
         initial_atoms = list(mmcif_chain.structure.get_atoms())
 
         mmcif_chain.remove_clashes()
+        mmcif_chain.remove_low_plddt()
         final_atoms = list(mmcif_chain.structure.get_atoms())
 
         assert len(final_atoms) < len(initial_atoms)
@@ -35,7 +36,7 @@ def test_remove_clashes():
 
 def test_remove_low_plddt():
     for chain in ['B', 'C']:
-        mmcif_chain = MmcifChainFiltered(cif_file, "TEST", chain)
+        mmcif_chain = MmcifChainFiltered(pdb_file, "TEST", chain)
         initial_atoms = list(mmcif_chain.structure.get_atoms())
 
         mmcif_chain.remove_low_plddt()
