f02433084a1a32248aa53beeaaea4375894141a1
Dima Molodenskiy <dmolodenskiy@embl-hamburg.de>
2024-02-26 15:18:19 +0100

    submit 4 slurm jobs with and without mmseqs2

--- test/test_features.py
+++ test/test_features.py
@@ -12,9 +12,11 @@ class TestCreateIndividualFeaturesWithTemplates(absltest.TestCase):
         super().setUp()
         self.temp_dir = tempfile.TemporaryDirectory()
         self.TEST_DATA_DIR = Path(self.temp_dir.name)
-        original_test_data_dir = Path(__file__).parent / "test_data" / "true_multimer"
-        shutil.copytree(original_test_data_dir, self.TEST_DATA_DIR, dirs_exist_ok=True)
-        (self.TEST_DATA_DIR / 'features').mkdir(parents=True, exist_ok=True)
+        self.original_test_data_dir = Path(__file__).parent / "test_data" / "true_multimer"
+        shutil.copytree(self.original_test_data_dir, self.TEST_DATA_DIR, dirs_exist_ok=True)
+
+        # Ensure the fastas directory is specifically mentioned for clarity and correctness
+        self.fasta_dir = self.TEST_DATA_DIR / 'fastas'
 
     def tearDown(self):
         self.temp_dir.cleanup()
@@ -29,15 +31,17 @@ class TestCreateIndividualFeaturesWithTemplates(absltest.TestCase):
 #SBATCH -N 1
 #SBATCH --ntasks=8
 #SBATCH --mem=32000
-source activate AlphaPulldown
+module load Anaconda3
+eval "$(conda shell.bash hook)"
+conda activate AlphaPulldown
 
 python {run_features_generation.__file__} \\
     --use_precomputed_msas True \\
     --save_msa_files True \\
     --skip_existing True \\
-    --data_dir {self.TEST_DATA_DIR} \\
+    --data_dir /scratch/AlphaFold_DBs/2.3.2 \\
     --max_template_date 3021-01-01 \\
-    --fasta_paths {self.TEST_DATA_DIR}/fastas/3L4Q_A.fasta,{self.TEST_DATA_DIR}/fastas/3L4Q_C.fasta \\
+    --fasta_paths {self.original_test_data_dir}/fastas/3L4Q_A.fasta,{self.original_test_data_dir}/fastas/3L4Q_C.fasta \\
     --output_dir {self.TEST_DATA_DIR}/features \\
     --seq_index $SLURM_ARRAY_TASK_ID \\
     --use_mmseqs2 $(if [ $SLURM_ARRAY_TASK_ID -eq 1 ] || [ $SLURM_ARRAY_TASK_ID -eq 3 ]; then echo True; else echo False; fi)
@@ -50,10 +54,10 @@ python {run_features_generation.__file__} \\
     def test_run_features_generation_all_parallel(self):
         slurm_script_file = self.generate_slurm_script()
         print(f"SLURM script generated at: {slurm_script_file}")
-        with open (slurm_script_file, "r") as file:
+        with open(slurm_script_file, "r") as file:
             print(file.read())
         # Uncomment the next line if you actually want to submit the job
-        # subprocess.run(['sbatch', '--array=1-4', str(slurm_script_file)], check=True)
+        subprocess.run(['sbatch', '--array=1-4', str(slurm_script_file)], check=True)
 
 if __name__ == '__main__':
     absltest.main()
