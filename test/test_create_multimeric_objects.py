e8f06ff17f3ef63e20cffbd6c70c57b5bec8a751
Geoffrey Yu <geoffrey.yu96@icloud.com>
2024-03-04 18:25:16 +0100

    updated unittest

--- test/test_create_multimeric_objects.py
+++ test/test_create_multimeric_objects.py
@@ -13,16 +13,15 @@ class TestCreateMultimericObject(unittest.TestCase):
     def setUp(self) -> None:
         self.monomer1 = pickle.load(open("./example_data/H1142_A.pkl", "rb"))
         self.monomer2 = pickle.load(open("./example_data/H1142_B.pkl", "rb"))
-
+    
     def test_1_initiate_default_multimericobject(self) -> MultimericObject:
         multimer_obj = MultimericObject([self.monomer1, self.monomer2])
         return multimer_obj
     
-    @unittest.expectedFailure
     def test_1_initiate_multimericobject_without_msa_pairing(self) -> MultimericObject:
         multimer_obj = MultimericObject([self.monomer1, self.monomer2],pair_msa=False)
         return multimer_obj
-    
+
     def test_2_check_residue_indexes(self):
         multimer_obj = self.test_1_initiate_default_multimericobject()
         seq_1_length = self.monomer1.feature_dict['seq_length'][0]
