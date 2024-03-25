6e06b384ec7c5ce0e9dd041adad5ff729a9b24b2
Geoffrey Yu <geoffrey.yu96@icloud.com>
2024-03-25 18:22:38 +0100

    update job clustring script to accomodate new create_combindations

--- alphapulldown/scripts/split_jobs_into_clusters.py
+++ alphapulldown/scripts/split_jobs_into_clusters.py
@@ -4,11 +4,12 @@ from alphapulldown.utils.create_combinations import process_files
 from alphapulldown.utils.modelling_setup import parse_fold, create_custom_info, create_interactors
 from alphapulldown.objects import MultimericObject
 import pandas as pd
-from typing import List
+from typing import List, Tuple
 from sklearn.cluster import MeanShift, estimate_bandwidth
 import matplotlib.pyplot as plt
 import numpy as np
-
+import logging
+logger = logging.getLogger(__name__)
 
 def create_multimer_objects(args):
     data = create_custom_info(args.parsed_input)
@@ -21,7 +22,8 @@ def profile_all_jobs_and_cluster(all_folds: List[str], args):
     output = {"name": [],
               "msa_depth": [],
               "seq_length": []}
-    for i in all_folds:
+    total_num = len(all_folds)
+    for idx, i in enumerate(all_folds):
         args.input = i
         args = parse_fold(args)
         multimer = create_multimer_objects(args)
@@ -29,6 +31,9 @@ def profile_all_jobs_and_cluster(all_folds: List[str], args):
         output['name'].append(i)
         output['msa_depth'].append(msa_depth)
         output['seq_length'].append(seq_length)
+        progress = ((idx+1)*100)/total_num
+        if idx % 10 ==0:
+            logger.info(f"Finished {idx +1} out of {total_num} {progress:3f} completed")
     return pd.DataFrame.from_dict(output)
 
 
@@ -59,6 +64,19 @@ def plot_clustering_result(X : np.array, labels : List[float | int],
         f"Total number of jobs: {total_num} Estimated number of clusters: {n_clusters_}")
     plt.savefig(f"{output_dir}/clustered_prediction_jobs.png")
 
+def write_individual_job_cluster(all_jobs : pd.DataFrame,
+                                 labels : list, output_dir : str) -> None:
+    unique_labels = np.unique(labels)
+    X = all_jobs.loc[:, ['seq_length', 'msa_depth']].values
+    for i, label in enumerate(unique_labels):
+        is_member = labels == label
+        max_seq_length, max_msa_depth = max(X[is_member, 0]), max(X[is_member, 1])
+        members = all_jobs[is_member]['name'].tolist()
+        file_name = f"{output_dir}/job_cluster{i+1}_{int(max_seq_length)}_{int(max_msa_depth)}.txt"
+        with open(file_name, "w") as outfile:
+            for m in members:
+                print(m, file=outfile)
+            outfile.close()
 
 def cluster_jobs(all_folds, args):
     print(f"start creating all jobs")
@@ -70,7 +88,8 @@ def cluster_jobs(all_folds, args):
     cluster = MeanShift(bandwidth=estimated_bandwidth)
     result = cluster.fit(X)
     labels, cluster_centres = result.labels_, result.cluster_centers_
-    plot_clustering_result(X, labels, cluster_centres)
+    write_individual_job_cluster(all_jobs, labels, args.output_dir)
+    plot_clustering_result(X, labels, cluster_centres, args.output_dir)
 
 
 def main():
@@ -93,6 +112,14 @@ def main():
         help="protein list files"
     )
     parser.add_argument(
+        "--mode",
+        dest="mode",
+        type=str,
+        default="pulldown",
+        required=True,
+        help="protein list files"
+    )
+    parser.add_argument(
         "--features_directory",
         dest="features_directory",
         type=str,
@@ -109,16 +136,24 @@ def main():
         help="output directory"
     )
     args = parser.parse_args()
-    args.mode = "custom"
     protein_lists = args.protein_lists
-
-    buffer = io.StringIO()
-    _ = process_files(input_files=protein_lists, output_path=buffer)
-    buffer.seek(0)
-    all_folds = buffer.readlines()
+    if args.mode == "all_vs_all":
+        protein_lists = [args.protein_lists[0], args.protein_lists[0]]
+    # buffer = io.StringIO()
+    import time
+    start = time.time()
+    all_combinations = process_files(input_files=protein_lists, output_path="buffer")
+    # buffer.seek(0)
+    # all_folds = buffer.readlines()
+    all_folds = ["+".join(combo) for combo in all_combinations]
     all_folds = [x.strip().replace(",", ":") for x in all_folds]
-    # all_folds = [x.strip().replace(";", "+") for x in all_folds]
+    all_folds = [x.strip().replace(";", "+") for x in all_folds]
+    end = time.time()
+    diff1 = end - start 
     cluster_jobs(all_folds, args)
+    end = time.time()
+    diff2 = end - start 
+    logger.info(f"process_files steps takes {diff1}s and total time is: {diff2}")
 
 
 if __name__ == "__main__":
