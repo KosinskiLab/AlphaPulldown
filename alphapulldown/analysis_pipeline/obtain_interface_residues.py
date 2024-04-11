5296a5847a06df7e76724e3934248d66a1c04837
Geoffrey Yu <geoffrey.yu96@icloud.com>
2024-04-11 13:41:01 +0200

    added script that obtains interface residues

---
+++ alphapulldown/analysis_pipeline/obtain_interface_residues.py
@@ -0,0 +1,52 @@
+""" 
+    A script that survey all interfaces and obtain all the residues on the interface
+
+    Copyright (c) 2024 European Molecular Biology Laboratory
+
+    Author:Dingquan Yu <dingquan.yu@embl-hamburg.de>
+"""
+from biopandas.pdb import PandasPdb
+import pandas as pd 
+from itertools import combinations
+
+class PDBAnalyser:
+    """
+    A class that store pandas dataframe of all the information 
+    of the residues in a PDB file 
+    """
+    def __init__(self, pdb_file_path: str) -> None:
+        self.pdb = PandasPdb().read_pdb(pdb_file_path)
+        self.pdb_df = self.pdb.df['ATOM']
+        self.chain_combinations = {}
+        self.get_all_combinations_of_chains()
+        pass
+    
+    def get_all_combinations_of_chains(self):
+        """A method that get all the paiwise combination of chains"""
+        unique_chain_ids = pd.unique(self.pdb_df.chain_id)
+        if len(unique_chain_ids) > 1:
+            chain_combinations = combinations(unique_chain_ids, 2)
+            for idx, combo in enumerate(chain_combinations):
+                self.chain_combinations.update(
+                    {f"interface_{idx+1}" : combo}
+                )
+        else:
+            self.chain_combinations = unique_chain_ids
+    
+    def retrieve_C_beta_coords(self, chain_df: pd.DataFrame) -> pd.DataFrame:
+        """
+        A method that retrieve the x,y,z coords of the C beta atoms of one chain
+        
+        Args:
+            chain_df : a pandas dataframe that belongs to one chain
+
+        Return:
+            a pandas dataframe of all C-beta atoms
+        Note:
+        Will retrieve C-alpha atom coords if the residue is a glycine
+        """
+        return chain_df.loc[(chain_df['atom_name'] == 'CB') or ((chain_df['residue_name'] == 'GLY') and (chain_df['atom_name'] == 'CA'))]
+
+    def obtain_interface_residues(self, chain_df_1: pd.DataFrame, chain_df_2: pd.DataFrame, cutoff:int = 12):
+        """A method that get all the residues on the interface within the cutoff"""
+        
