9ac4605c89cf5ba1debd7aa00b39ad82f575a326
Dima Molodenskiy <dmolodenskiy@embl-hamburg.de>
2024-06-13 13:50:19 +0200

    Save protein without index to meta.json

--- alphapulldown/scripts/create_individual_features.py
+++ alphapulldown/scripts/create_individual_features.py
@@ -337,7 +337,7 @@ def process_multimeric_features(feat, idx):
             temp_dir, protein, template_paths, chains)
         create_arguments(local_path_to_custom_db)
 
-        flags_dict.update({f"protein_{idx}": protein, f"multimeric_templates_{idx}": template_paths,
+        flags_dict.update({f"protein": protein, f"multimeric_templates_{idx}": template_paths,
                            f"multimeric_chains_{idx}": chains})
 
         if not FLAGS.use_mmseqs2:
