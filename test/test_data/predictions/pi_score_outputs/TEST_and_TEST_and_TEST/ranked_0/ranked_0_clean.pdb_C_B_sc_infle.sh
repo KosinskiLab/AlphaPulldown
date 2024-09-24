e5f25a428a38ba0d7140a6fb808ab9f69ad0fa4d
Dima Molodenskiy <dmolodenskiy@embl-hamburg.de>
2024-09-24 15:57:54 +0200

    New dir for testing pi_score extraction scripts

---
+++ test/test_data/predictions/pi_score_outputs/TEST_and_TEST_and_TEST/ranked_0/ranked_0_clean.pdb_C_B_sc_infle.sh
@@ -0,0 +1,7 @@
+sc XYZIN ranked_0_clean.pdb <<eof 
+MOLECULE 1
+CHAIN C
+MOLECULE 2
+CHAIN B
+END
+eof
---
+++ test/test_data/predictions/pi_score_outputs/TEST_and_TEST_and_TEST/ranked_0/ranked_0_clean.pdb_D_C_sc_infle.sh
@@ -0,0 +1,7 @@
+sc XYZIN ranked_0_clean.pdb <<eof 
+MOLECULE 1
+CHAIN D
+MOLECULE 2
+CHAIN C
+END
+eof
