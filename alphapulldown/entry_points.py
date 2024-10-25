09397f3ee409b14ce5de29cc6f2b716d0d844b70
Dima Molodenskiy <dmolodenskiy@embl-hamburg.de>
2024-10-25 18:12:50 +0200

    Keep *.py extension for all scripts

--- alphapulldown/entry_points.py
+++ alphapulldown/entry_points.py
@@ -8,17 +8,22 @@ def run_multimer_jobs():
     from alphapulldown.scripts import run_multimer_jobs
     app.run(run_multimer_jobs.main)
 
-def create_notebook():
-    from absl import app
-    from alphapulldown.analysis_pipeline import create_notebook
-    app.run(create_notebook.main)
-
 def convert_to_modelcif():
     from absl import app
     from alphapulldown.scripts import convert_to_modelcif
     app.run(convert_to_modelcif.main)
 
-def truncate_pickles_entry():
+def truncate_pickles():
     from absl import app
     from alphapulldown.scripts import truncate_pickles
     app.run(truncate_pickles.main)
+
+def create_notebook():
+    from absl import app
+    from alphapulldown.analysis_pipeline import create_notebook
+    app.run(create_notebook.main)
+
+def get_good_interpae():
+    from absl import app
+    from alphapulldown.analysis_pipeline import get_good_inter_pae
+    app.run(get_good_inter_pae.main)
